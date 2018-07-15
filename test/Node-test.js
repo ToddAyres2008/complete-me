8dfdcabe3b8b4f85b203b8166aeb8cfae17d82df
Todd Ayres <todd.ayres@ymail.com>
2018-07-15 15:19:00 -0600

    Add insert tests

--- test/Node-test.js
+++ test/Node-test.js
@@ -15,12 +15,12 @@ describe('Node', function() {
 
   it('should be able to accept data', function() {
     let newNode = new Node('dog')
-    assert.equal(newNode.data, 'dog')
+    assert.deepEqual(newNode.data, 'dog')
   })
 
   it('should begin with an empty child object', function() {
     let newNode = new Node()
-    assert.equal(newNode.child, {})
+    assert.deepEqual(newNode.child, {})
   });
 
   it('should begin with a completeWord value of null', function() {
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,10 +1,40 @@
 const { assert } = require('chai');
 const Trie = require('../lib/Trie.js');
+const Node = require('../lib/Node.js');
 
 describe('Trie', function() {
 
-it('should have default properties', function () {
-    var newTrie = new Trie();
-    assert.equal(newTrie, Trie());
+it('should have its root establish a new Node', function () {
+  let newTrie = new Trie();
+  let newNode = new Node();
+  assert.deepEqual(newTrie.root, newNode)
   });
+
+it('should begin with an intial count of 0', function(){
+  let newTrie = new Trie();
+  assert.deepEqual(newTrie.count, 0)
+  });
+
+it('should increase the counter completed word', function () {
+  let newTrie = new Trie();
+  newTrie.insert('dog');
+  newTrie.insert('cat');
+  assert.deepEqual(newTrie.count, 2)
 });
+
+
+it('should add a child node to complete a word', function() {
+  let newTrie = new Trie();
+  newTrie.insert('dog')
+  assert.equal(newTrie.root.completeWord, 'dog')
+  });
+
+it('should not populate completeWord value, if no string passed', function() {
+  let newTrie = new Trie();
+  newTrie.insert()
+  assert.equal(newTrie.root.completeWord, null)
+});
+
+});
+
+
