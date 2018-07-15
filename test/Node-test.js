20bb4d47d7ee9d888dfce4c46565d0e8c6d989fc
Todd Ayres <todd.ayres@ymail.com>
2018-07-15 13:09:19 -0600

    Fix node tests

--- test/Node-test.js
+++ test/Node-test.js
@@ -5,7 +5,27 @@ const Node = require('../lib/Node.js');
 describe('Node', function() {
 
   it('should instantiate a new Node', function () {
-    var newNode = new Node();
+    let newNode = new Node();
     assert.isObject(newNode);
   });
+
+  it('should have a data property default of null', function() {
+    assert.equal(Node.data, null)
+  });
+
+  it('should be able to accept data', function() {
+    let newNode = new Node('dog')
+    assert.equal(newNode.data, 'dog')
+  })
+
+  it('should begin with an empty child object', function() {
+    let newNode = new Node()
+    assert.equal(newNode.child, {})
+  });
+
+  it('should begin with a completeWord value of null', function() {
+    let newNode = new Node()
+    assert.equal(Node.data, null)
+  });
+
 });
