0f93361301a5b7b83e8e2395cd6683ab0ac6b79a
Todd Ayres <todd.ayres@ymail.com>
2018-07-15 19:57:38 -0600

    Fix suggested test

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -4,36 +4,42 @@ const Node = require('../lib/Node.js');
 
 describe('Trie', function() {
 
-it('should have its root establish a new Node', function () {
-  let newTrie = new Trie();
-  let newNode = new Node();
-  assert.deepEqual(newTrie.root, newNode)
+  it('should have its root establish a new Node', function () {
+    let newTrie = new Trie();
+    let newNode = new Node();
+    assert.deepEqual(newTrie.root, newNode)
+    });
+
+  it('should begin with an intial count of 0', function(){
+    let newTrie = new Trie();
+    assert.deepEqual(newTrie.count, 0)
+    });
+
+  it('should increase the counter completed word', function () {
+    let newTrie = new Trie();
+    newTrie.insert('dog');
+    newTrie.insert('cat');
+    assert.deepEqual(newTrie.count, 2)
   });
 
-it('should begin with an intial count of 0', function(){
-  let newTrie = new Trie();
-  assert.deepEqual(newTrie.count, 0)
+  it('should add a child node to complete a word', function() {
+    let newTrie = new Trie();
+    newTrie.insert('dog')
+    assert.equal(newTrie.root.completeWord, 'dog')
+    });
+
+  it('should not populate completeWord value, if no string passed', function() {
+    let newTrie = new Trie();
+    newTrie.insert()
+    assert.equal(newTrie.root.completeWord, null)
   });
 
-it('should increase the counter completed word', function () {
-  let newTrie = new Trie();
-  newTrie.insert('dog');
-  newTrie.insert('cat');
-  assert.deepEqual(newTrie.count, 2)
-});
-
-
-it('should add a child node to complete a word', function() {
-  let newTrie = new Trie();
-  newTrie.insert('dog')
-  assert.equal(newTrie.root.completeWord, 'dog')
-  });
-
-it('should not populate completeWord value, if no string passed', function() {
-  let newTrie = new Trie();
-  newTrie.insert()
-  assert.equal(newTrie.root.completeWord, null)
-});
+  it('should return suggestions', function() {
+    let newTrie = new Trie();
+    newTrie.insert('zebra');
+    newTrie.suggest('z');
+    assert.deepEqual(newTrie.possibleWords, ['zebra'])
+  })
 
 });
 
